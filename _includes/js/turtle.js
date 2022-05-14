f62141e71b0e8bfa0a9f73e44cee016f2541fb67
jareyes <josh@joshreyes.com>
2022-05-14 14:22:49 -0400

    Pen up bug

--- _includes/js/turtle.js
+++ _includes/js/turtle.js
@@ -51,7 +51,13 @@ class Turtle {
   }
 
   penDown() {
-    this.isPenDown = true;
+    this.isPenDown = true;xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
   }
 
   penUp() {
@@ -71,6 +77,7 @@ class Turtle {
     this.x += Math.cos(this.heading) * steps;
     this.y += Math.sin(this.heading) * steps;
     if(!this.isPenDown) {
+      refreshTurtle(this);
       return;
     }
     // Draw the line
